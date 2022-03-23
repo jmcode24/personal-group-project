d4322491793af967fd9fd3986de8f3dc4ca3da7e
Jamil Mohammed <zeus2tee@gmail.com>
2022-03-23 16:52:57 +0000

    added an alert

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Container, Form, InputGroup, FormControl, Button } from 'react-bootstrap';
+import { Container, Form, InputGroup, FormControl, Button, Alert } from 'react-bootstrap';
 import { BiCollapse, BiPencil } from 'react-icons/bi';
 import { GiMoneyStack } from 'react-icons/gi';
 import { useDispatch } from 'react-redux';
@@ -10,6 +10,8 @@ function ExpenseForm() {
   const [item, setItem] = useState('');
   const [amount, setAmount] = useState('');
   const [category, setCategory ] = useState('');
+  const [error, setError] = useState(true);
+  const [success, setSuccess] = useState(true);
 
   const dispatch = useDispatch('');
 
@@ -37,19 +39,31 @@ function ExpenseForm() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    let newExpense = {
-      id: uuid(),
-      date: new Date(),
-      item: item,
-      amount: amount * 1,
-      category: category,
-    };
+    if (!item || !amount || !category) {
+      setError(false);
+      setTimeout(() => {
+        setError(true);
+      }, 2000);
+    } else {
+      let newExpense = {
+        id: uuid(),
+        date: new Date(),
+        item: item,
+        amount: amount * 1,
+        category: category,
+      };
 
-    dispatch(addExpenseAction(newExpense));
-
-    setAmount('');
-    setItem('');
-    setCategory('');
+      setSuccess(false);
+      setTimeout(() => {
+        setSuccess(true);
+      }, 3000);
+  
+      dispatch(addExpenseAction(newExpense));
+  
+      setAmount('');
+      setItem('');
+      setCategory('');
+    }
   };
 
   return (
@@ -57,9 +71,11 @@ function ExpenseForm() {
       <Container>
         <h1 className='text-secondary mt-2 mb-2'>Expense Tracker</h1>
         <Form onSubmit={handleSubmit}>
+        {!error ? <Alert variant='danger'>Check all fields</Alert> : '' }
+        {!success ? <Alert variant='success'>Expense added</Alert> : '' }
           <InputGroup className="mb-3">
             <InputGroup.Text id="basic-addon1" className='fw-bold text-success'><BiPencil /></InputGroup.Text>
-            <FormControl type="text" required
+            <FormControl type="text"
               value={item} onChange={handleItemChange}
               placeholder="Name Of Item"
               aria-label="name of item"
@@ -69,7 +85,7 @@ function ExpenseForm() {
           </InputGroup>
           <InputGroup className="mb-3">
             <InputGroup.Text id="basic-addon1" className='fw-bold text-success'><GiMoneyStack /></InputGroup.Text>
-            <FormControl type="number" required
+            <FormControl type="number" 
               value={amount} onChange={handleAmountChange}
               placeholder="Amount Paid"
               aria-label="amount paid"
@@ -79,7 +95,7 @@ function ExpenseForm() {
           </InputGroup>
           <InputGroup className="mb-3">
             <InputGroup.Text id="basic-addon1" className='fw-bold text-success'><BiCollapse /></InputGroup.Text>
-              <Form.Select className='text-white' aria-label="Default select example" required value={category} onChange={handleChange}>
+              <Form.Select className='text-white' aria-label="Default select example" value={category} onChange={handleChange}>
                 <option>Click & Select Category</option>
                 {options.map((option, index) => {
                   return (
